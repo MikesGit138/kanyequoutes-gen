294bc8e8db31fc6fd46f4cd66010aa30d21176f4
Michael Leighton <michael138leighton@gmail.com>
2022-09-22 13:39:28 -0500

    styling changes

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ function newQuote(){
     const fetchQuotes = async() => {
         const res = await fetch('https://api.kanye.rest')
         const data = await res.json()
-        // console.log(data.quote)
+        if (data.quote.length > 150 ) document.querySelector('.section').style.width = '15em';
         document.querySelector('.quotes').innerHTML = data.quote
-        console.log(data)
- 
     }
     fetchQuotes()
 }
